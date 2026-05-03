c6dad6aca773c61685dfc8befa2b4b37ecc3e443
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2026-05-03 13:45:45 -0300

    add metodos

--- src/main/java/com/poo/Carro.java
+++ src/main/java/com/poo/Carro.java
@@ -6,9 +6,59 @@ public class Carro {
     String modelo;
     String cor;
     int anoFabricacao;
+    int anosUsado;
+    double precoCompra;
 
     // Composição - Relação Tem- Um
     Pessoa proprietario = new Pessoa(); // evitando NullPointer
+
+    void imprimirResumoDepreciacao(){
+        double valorRevenda = calculoValorRevenda();
+        int tempoDeUsoCarro = calculaTempoDeUso();
+
+        if(precoCompra <= 0){
+            System.out.println("Carro com preço de compra zerado. Não foi possível imprimir o tempo de depreciação");
+            return; // sairá de do método
+            //ou por exemplo , no documento informar algo importante
+        }
+
+        System.out.println("Tempo de uso " + tempoDeUsoCarro);
+        System.out.println("Valor de revenda " + valorRevenda);
+    }
+
+    double calcularIpva(){
+
+        double tempoDeUsoEmAnos = calculoValorRevenda();
+        if(tempoDeUsoEmAnos * 0.04 >= 10){
+            // se cair aqui , estará isento e sairá de todo o método pois está com return
+            return 0; // isento
+        }
+        if (tempoDeUsoEmAnos <= 0){
+            System.out.println("Não foi possível calcular o IPVA pois o tempo de uso está zerado");
+            return 0.0;
+        }
+        return tempoDeUsoEmAnos * 0.04;
+    }
+
+    int calculaTempoDeUso(){
+        int tempoDeUsoEmAnos = anosUsado - anoFabricacao;
+        return  tempoDeUsoEmAnos;
+    }
+
+    double calculoValorRevenda() {
+        int valorUso = calculaTempoDeUso();
+
+        int vidaUtilEmAnos = 20;
+        double valorRevenda = (precoCompra / vidaUtilEmAnos) * (vidaUtilEmAnos - valorUso);
+
+        if(valorRevenda < 0 && valorUso < 0){
+            valorRevenda =0;
+            valorUso = 0;
+        }
+
+        return valorRevenda;
+    }
+
 
     public String getFabricante() {
         return fabricante;
@@ -42,6 +92,22 @@ public class Carro {
         this.anoFabricacao = anoFabricacao;
     }
 
+    public double getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(double precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public int getAnosUsado() {
+        return anosUsado;
+    }
+
+    public void setAnosUsado(int anosUsado) {
+        this.anosUsado = anosUsado;
+    }
+
     @Override
     public String toString() {
         return "Carro{" +
@@ -49,6 +115,8 @@ public class Carro {
                 ", modelo='" + modelo + '\'' +
                 ", cor='" + cor + '\'' +
                 ", anoFabricacao=" + anoFabricacao +
+                ", anosUsado=" + anosUsado +
+                ", precoCompra=" + precoCompra +
                 ", proprietario=" + proprietario +
                 '}';
     }
