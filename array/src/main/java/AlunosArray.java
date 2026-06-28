9f6b982fdea652fd7d13cfb476cd5e9dcc541a5f
JoaoLlucaxs <queirozjoaolucas8@gmail.com>
2026-06-28 11:59:52 -0300

    arrays e manipulações

---
+++ array/src/main/java/AlunosArray.java
@@ -0,0 +1,22 @@
+public class AlunosArray {
+    public static void main(String[] args) {
+
+        Turma turmab = new Turma();
+
+        turmab.identificacao = "Maternal B";
+        turmab.nomeProfessora = "Renata";
+        turmab.alunos= new Aluno[3];
+
+        // método 2
+        turmab.alunos[0] = new Aluno();
+        turmab.alunos[0].nome = "João";
+        turmab.alunos[0].idade = 5;
+
+        // método 1
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "Jesuita";
+        aluno1.idade = 4;
+
+        turmab.alunos[1]=aluno1;
+    }
+}
---
+++ array/src/main/java/Ordenacao.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Ordenacao {
+    public static void main(String[] args) {
+
+        int[] notas = {8,5,4,10 ,9};
+
+        Arrays.sort(notas);
+
+        System.out.println(Arrays.toString(notas));
+
+    }
+}
